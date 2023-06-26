426fae1e23c7df7f3b7996fa54acd476b44a0c4d
miyanew <dev38_shin@yahoo.co.jp>
2023-06-26 17:18:10 +0900

    The colors of today can be reversed.

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -7,41 +7,56 @@ require 'date'
 opt = OptionParser.new
 
 params = {}
-opt.on('-m [value]','this parameter is month') {|v| params[:month] = v } 
-opt.on('-y [value]','this parameter is year') {|v| params[:year] = v }
+opt.on('-m [value]', 'this parameter is month') { |v| params[:month] = v }
+opt.on('-y [value]', 'this parameter is year') { |v| params[:year] = v }
 opt.parse!(ARGV)
 
 def stdout_alert
-  puts 'Alert'
+  puts 'Usage: cal [-m month] [-y year]'
   exit
 end
 
-# -y オプション単独使用は許容しない
-if params[:month].nil? && !params[:year].nil?
-  stdout_alert
-end
+# -y オプションの単独使用を許容しない
+stdout_alert if params[:month].nil? && !params[:year].nil?
 
+# コマンド引数が未入力のときはシステム日付をセットする
 params[:month] ||= Date.today.month.to_s
 params[:year] ||= Date.today.year.to_s
 
+# コマンド引数で指定した対象月のカレンダーを表示させる関数
+#  Macローカル環境の/usr/bin/calの出力形式を模して表示する
+#  表示列は日曜日から始まり土曜日で終わる。行範囲は6行を確保する。対象月以外の日付は空白にする
+#  今日の日付の部分の色を反転させて表示する
 def stdout_calender(params)
-  lbl_month_year = "#{params[:month]}月 #{params[:year]}".center(20," ")
+  month_target = params[:month]
+  year_target = params[:year]
+  space = ' '
 
-  lbl_weekdays = ["日","月","火","水","木","金","土"].join(" ")
-
-  num_of_displayrow = 6
-  num_of_displaycol = Date::DAYNAMES.size
-  num_of_leading_blank = Date.new(params[:year].to_i,params[:month].to_i,1).wday
-  num_of_trailing_blank = num_of_displaycol * num_of_displayrow - Date.new(params[:year].to_i, params[:month].to_i, -1).day - num_of_leading_blank
-  
-  day_of_month = (Date.new(params[:year].to_i,params[:month].to_i,1)..Date.new(params[:year].to_i,params[:month].to_i,-1)).to_a
-  day_of_month = day_of_month.map { |ymd| ymd.day.to_s.rjust(2," ") }
-  day_of_month = Array.new(num_of_leading_blank," ".rjust(2," ")) + day_of_month 
-  day_of_month = day_of_month + (Array.new(num_of_trailing_blank," ".rjust(2," ")))
-
+  width_month_display = 20
+  lbl_month_year = "#{month_target}月 #{year_target}".center(width_month_display, space)
   puts lbl_month_year
+
+  lbl_weekdays = %w[日 月 火 水 木 金 土].join(space)
   puts lbl_weekdays
-  num_of_displayrow.times { |num_row| puts day_of_month[num_row * num_of_displaycol,num_of_displaycol].join(" ")}
- end
+
+  day_from = Date.new(year_target.to_i, month_target.to_i, 1)
+  day_from = day_from.prev_day(1) while day_from.wday != 0
+  num_of_displayrows = 6
+  day_to = day_from + (Date::DAYNAMES.length * num_of_displayrows - 1)
+
+  width_date_display = 2
+  (day_from..day_to).each do |day|
+    if day == Date.today
+      printf("\e[7m%s\e[0m", day.day.to_s.rjust(width_date_display, space))
+      print(space)
+    elsif day.month.to_s == month_target
+      print(day.day.to_s.rjust(width_date_display, space).concat(space))
+    else
+      print(space.rjust(width_date_display, space).concat(space))
+    end
+
+    print "\n" if day.wday == 6
+  end
+end
 
 stdout_calender(params)
