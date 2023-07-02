d3e05d5752eb1f403dfd41cd0957334ff9b0dea8
miyanew <dev38_shin@yahoo.co.jp>
2023-07-03 03:02:08 +0900

    Update to reflect points raised. And Fix problem where today was displayed outside of the current month.

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -7,8 +7,8 @@ require 'date'
 opt = OptionParser.new
 
 params = {}
-opt.on('-m [value]', 'this parameter is month') { |v| params[:month] = v }
-opt.on('-y [value]', 'this parameter is year') { |v| params[:year] = v }
+opt.on('-m [value]', 'this parameter is month') { |v| params[:month] = v.to_i }
+opt.on('-y [value]', 'this parameter is year') { |v| params[:year] = v.to_i }
 opt.parse!(ARGV)
 
 def stdout_alert
@@ -20,43 +20,51 @@ end
 stdout_alert if params[:month].nil? && !params[:year].nil?
 
 # コマンド引数が未入力のときはシステム日付をセットする
-params[:month] ||= Date.today.month.to_s
-params[:year] ||= Date.today.year.to_s
+params[:month] ||= Date.today.month
+params[:year] ||= Date.today.year
 
 # コマンド引数で指定した対象月のカレンダーを表示させる関数
 #  Macローカル環境の/usr/bin/calの出力形式を模して表示する
 #  表示列は日曜日から始まり土曜日で終わる。行範囲は6行を確保する。対象月以外の日付は空白にする
 #  今日の日付の部分の色を反転させて表示する
+WIDTH_MONTH_DISPLAY = 20
+NUM_OF_DISPLAYROWS = 6
+NUM_OF_DISPLAYCOLS = Date::DAYNAMES.length
+WIDTH_DATE_DISPLAY = 2
+
 def stdout_calender(params)
   month_target = params[:month]
   year_target = params[:year]
   space = ' '
 
-  width_month_display = 20
-  lbl_month_year = "#{month_target}月 #{year_target}".center(width_month_display, space)
+  lbl_month_year = "#{month_target}月 #{year_target}".center(WIDTH_MONTH_DISPLAY, space)
   puts lbl_month_year
 
   lbl_weekdays = %w[日 月 火 水 木 金 土].join(space)
   puts lbl_weekdays
 
-  day_from = Date.new(year_target.to_i, month_target.to_i, 1)
-  day_from = day_from.prev_day(1) while day_from.wday != 0
-  num_of_displayrows = 6
-  day_to = day_from + (Date::DAYNAMES.length * num_of_displayrows - 1)
+  day_from = Date.new(year_target, month_target, 1)
+  day_from = day_from.prev_day(1) until day_from.sunday?
+  day_to = day_from + (NUM_OF_DISPLAYCOLS * NUM_OF_DISPLAYROWS - 1)
 
-  width_date_display = 2
   (day_from..day_to).each do |day|
-    if day == Date.today
-      printf("\e[7m%s\e[0m", day.day.to_s.rjust(width_date_display, space))
-      print(space)
-    elsif day.month.to_s == month_target
-      print(day.day.to_s.rjust(width_date_display, space).concat(space))
+    if day.month == month_target
+      if day == Date.today
+        printf("\e[7m%s\e[0m", format_by_date(day.day.to_s))
+      else
+        print(format_by_date(day.day.to_s))
+      end
     else
-      print(space.rjust(width_date_display, space).concat(space))
+      print(format_by_date(space))
     end
-
-    print "\n" if day.wday == 6
+    print(space)
+    print("\n") if day.saturday?
   end
+end
+
+def format_by_date(date_text)
+  space = ' '
+  date_text.rjust(WIDTH_DATE_DISPLAY, space)
 end
 
 stdout_calender(params)
