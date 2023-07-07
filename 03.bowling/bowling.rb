91f59ed83f80ab4bc2b42df6f56db420ce0a6981
miyanew <dev38_shin@yahoo.co.jp>
2023-07-08 03:41:49 +0900

    Update to reflect point

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,28 +1,25 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: false
 
-score = ARGV[0]
+STRIKE_SCORES = 10
 
 # ボウリングの10フレーム分のスコアから合計得点を算出する
-def score_calculation(scores)
+def calculate_score(scores)
   # スコア を 倒したピン数 に変換する
   shots = []
   scores.each do |s|
     if s == 'X' && shots.size < (9 * 2) # 9フレーム目までのストライク
-      shots << 10
+      shots << STRIKE_SCORES
       shots << nil
     elsif s == 'X' # 10フレーム目のストライク
-      shots << 10
+      shots << STRIKE_SCORES
     else
       shots << s.to_i
     end
   end
 
   # 倒したピン数をフレーム毎に分ける。10フレーム目に3投した場合、11フレームが出現するので補正する。
-  frames = []
-  shots.each_slice(2) do |s|
-    frames << s
-  end
+  frames = shots.each_slice(2).map(&:compact)
 
   if frames.size > 10
     frames_tmp = frames[0..8]
@@ -33,15 +30,20 @@ def score_calculation(scores)
   # 倒したピンの総数とボーナス点を加算して合計点をだす。ボーナス点があるのは9フレーム目まで。
   point = 0
   frames.each_with_index do |frame, idx|
-    if (idx < 9) && (frame[0] == 10) # strike bonus
-      shots_future = frames[(idx + 1)..].flatten.delete_if(&:nil?)
-      point += shots_future[0..1].sum
-    elsif (idx < 9) && (frame[0] != 10) && (frame.sum == 10) # spare bonus
-      point += frames[idx + 1][0]
+    if idx < 9
+      if frame[0] == 10 # strike bonus
+        shots_future = frames[(idx + 1)..].flatten
+        point += shots_future[0..1].sum
+      elsif frame.sum == 10 # spare bonus
+        point += frames[idx + 1][0]
+      end
     end
-    point += frame.delete_if(&:nil?).sum
+    point += frame.sum
   end
-  p point
+  point
 end
 
-score_calculation(score.split(','))
+if $PROGRAM_NAME == __FILE__
+  score = ARGV[0]
+  p calculate_score(score.split(','))
+end
