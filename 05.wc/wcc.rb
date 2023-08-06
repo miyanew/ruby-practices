4673af1f717236c942bb2679d3cad4662dbf1b0f
miyanew <dev38_shin@yahoo.co.jp>
2023-08-06 22:10:53 +0900

    Clean up by refactoring

--- 05.wc/wcc.rb
+++ 05.wc/wcc.rb
@@ -5,6 +5,12 @@ require 'optparse'
 
 WIDTH_SUM_VALUES = 8
 
+# Rubyでつくるwcコマンド
+#   Done: コマンド引数からは、複数ファイルを指定可能にする
+#   Done: パイプラインからも入力可能にする
+#   Done: -lオプションを作る 行数をカウント
+#   Done: -wオプションを作る 単語数をカウント
+#   Done: -cオプションを作る バイト数をカウント
 def main(args)
   options = {}
   options[:filepaths] = args[:filepaths] ||= false
@@ -12,42 +18,45 @@ def main(args)
   options[:w]         = args[:w] ||= false
   options[:c]         = args[:c] ||= false
 
-  input_lines = []
-  if options[:filepaths].size.zero?
+  input_params = correct_inputs(options[:filepaths])
+  input_params = count_inputs(input_params)
+  show_result(input_params)
+end
+
+def correct_inputs(filepaths)
+  result = []
+  if filepaths.empty?
     stdin = {}
     stdin[:filepath] = nil
     stdin[:body] = readlines.map(&:chomp)
-    input_lines << stdin
+    result << stdin
   else
-    options[:filepaths].each do |fp|
+    filepaths.each do |fp|
       input_file = {}
       input_file[:filepath] = fp
-      input_file[:body] = File.open(fp){|f| f.readlines.map(&:chomp)}
-      input_lines << input_file
+      input_file[:body] = File.open(fp) { |f| f.readlines.map(&:chomp) }
+      result << input_file
     end
   end
-
-  input_lines.each do |line|
-    count_input_lines(line)
-  end
-
-  show_result(input_lines)
+  result
 end
 
-def count_input_lines(line_params)
-    rets = []
-    line_params[:body].to_a.map do |row|
+def count_inputs(input_params)
+  input_params.each do |input_param|
+    result_count_to_body = []
+
+    input_param[:body].map do |row|
       ret = {}
       ret[:lines_count] = 1
       ret[:words_count] = count_devide_by_space(row)
       ret[:bytes_count] = count_bytesize_with_linefeed(row)
-      rets << ret
+      result_count_to_body << ret
     end
 
-    line_params[:lines_count] = rets.map {|ret| ret[:lines_count]}.sum
-    line_params[:words_count] = rets.map {|ret| ret[:words_count]}.sum
-    line_params[:bytes_count] = rets.map {|ret| ret[:bytes_count]}.sum
-    line_params
+    input_param[:lines_count] = result_count_to_body.map { |ret| ret[:lines_count] }.sum
+    input_param[:words_count] = result_count_to_body.map { |ret| ret[:words_count] }.sum
+    input_param[:bytes_count] = result_count_to_body.map { |ret| ret[:bytes_count] }.sum
+  end
 end
 
 def count_devide_by_space(input_line)
@@ -59,25 +68,25 @@ def count_bytesize_with_linefeed(input_line)
   input_line.bytesize + "\n".bytesize
 end
 
-def show_result(input_lines)
-  input_lines.each do |input_line|
-    lines_sum = adjust_show_width(input_line[:lines_count])
-    words_sum = adjust_show_width(input_line[:words_count])
-    bytes_sum = adjust_show_width(input_line[:bytes_count])
+def show_result(count_results)
+  count_results.each do |count_result|
+    lines_sum = adjust_show_width(count_result[:lines_count])
+    words_sum = adjust_show_width(count_result[:words_count])
+    bytes_sum = adjust_show_width(count_result[:bytes_count])
 
     show_line = "#{lines_sum}#{words_sum}#{bytes_sum}"
-    show_line << " #{input_line[:filepath]}" unless input_line[:filepath].nil?
+    show_line << " #{count_result[:filepath]}" unless count_result[:filepath].nil?
     puts show_line
   end
 
-  if input_lines.size > 1
-    lines_sum_total = adjust_show_width(input_lines.map.sum { |line| line[:lines_count]})
-    words_sum_total = adjust_show_width(input_lines.map.sum { |line| line[:words_count]})
-    bytes_sum_total = adjust_show_width(input_lines.map.sum { |line| line[:bytes_count]})
+  return unless count_results.size > 1
 
-    show_line = "#{lines_sum_total}#{words_sum_total}#{bytes_sum_total} total"
-    puts show_line
-  end
+  lines_sum_total = adjust_show_width(count_results.map.sum { |line| line[:lines_count] })
+  words_sum_total = adjust_show_width(count_results.map.sum { |line| line[:words_count] })
+  bytes_sum_total = adjust_show_width(count_results.map.sum { |line| line[:bytes_count] })
+
+  show_line = "#{lines_sum_total}#{words_sum_total}#{bytes_sum_total} total"
+  puts show_line
 end
 
 def adjust_show_width(sum_string)
