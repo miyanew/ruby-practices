28a2e7f1f54bf11a482ad06753702c3c1e342b3b
miyanew <dev38_shin@yahoo.co.jp>
2023-08-19 21:13:16 +0900

    Clean up to issue raised

--- 05.wc/wcc.rb
+++ 05.wc/wcc.rb
@@ -14,30 +14,28 @@ def main(options)
 end
 
 def show_stats_for_pipe_input(options)
-  input_stats = [count_input(readlines.join)]
+  input_stats = [build_stats(readlines.join)]
   show_result(input_stats, options)
 end
 
 def show_stats_for_file_input(options)
-  input_stats_list = options[:filepaths].map do |fp|
-    input_stats = count_input(File.read(fp))
-    input_stats.merge(file_path: fp)
-  end
+  input_stats_list = options[:filepaths].map { |fp| build_stats(File.read(fp), fp) }
   show_result(input_stats_list, options)
   show_total(input_stats_list, options) if input_stats_list.size > 1
 end
 
-def count_input(input_text)
+def build_stats(input_text, file_path = '')
   {
     lines_count: input_text.lines.size,
     words_count: input_text.split("\s").size,
-    bytes_count: input_text.bytesize
+    bytes_count: input_text.bytesize,
+    file_path:
   }
 end
 
 def show_result(count_results, options)
   count_results.each do |count_result|
-    puts build_line(count_result, options, count_result[:file_path])
+    puts build_line(count_result, options)
   end
 end
 
@@ -45,26 +43,24 @@ def show_total(input_stats_list, options)
   totals = {
     lines_count: input_stats_list.sum { |ret| ret[:lines_count] },
     words_count: input_stats_list.sum { |ret| ret[:words_count] },
-    bytes_count: input_stats_list.sum { |ret| ret[:bytes_count] }
+    bytes_count: input_stats_list.sum { |ret| ret[:bytes_count] },
+    file_path: 'total'
   }
-  puts build_line(totals, options, 'total')
+  puts build_line(totals, options)
 end
 
-def build_line(count_result, options, counted_target)
+def build_line(count_result, options)
+  show_all = options.values_at(:l, :w, :c).none?
   line = ''
-  line += adjust_width(count_result[:lines_count]) if options[:l] || show_all?(options)
-  line += adjust_width(count_result[:words_count]) if options[:w] || show_all?(options)
-  line += adjust_width(count_result[:bytes_count]) if options[:c] || show_all?(options)
-  line += " #{counted_target}" unless counted_target.nil?
+  line += adjust_width(count_result[:lines_count]) if options[:l] || show_all
+  line += adjust_width(count_result[:words_count]) if options[:w] || show_all
+  line += adjust_width(count_result[:bytes_count]) if options[:c] || show_all
+  line += " #{count_result[:file_path]}" unless count_result[:file_path].nil?
   line
 end
 
 def adjust_width(sum)
   sum.to_s.rjust(WIDTH)
-end
-
-def show_all?(options)
-  !options[:l] && !options[:w] && !options[:c]
 end
 
 if $PROGRAM_NAME == __FILE__
